d9634f72dac5a82953f620d085ad73b740e63a5a
Magnuuus92 <Magnus92.kodehode@gmail.com>
2025-10-28 14:55:18 +0100

    linqtest

--- kombinere/Program.cs
+++ kombinere/Program.cs
@@ -2,39 +2,52 @@
 
 class Program
 {
-       static double getuserinput(string input)
-       {Console.WriteLine(input);
-       return Convert.ToDouble(Console.ReadLine());
-       }
-       static double addition(double in1, double in2){
-        Console.WriteLine("addition");
-    return in1 + in2;
+    static double getuserinput(string input)
+    {
+        Console.WriteLine(input);
+        try
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        catch (System.Exception)
+        {
+
+            return 0;
+        }
     }
-    static double addition(double in1, double in2, double in3){
+    static double addition(double in1, double in2)
+    {
+        Console.WriteLine("addition");
+        return in1 + in2;
+    }
+    static double addition(double in1, double in2, double in3)
+    {
         return in1 + in2 + in3;
     }
-    static double addition(double in1, double in2, double in3, double in4){
+    static double addition(double in1, double in2, double in3, double in4)
+    {
         return in1 + in2 + in3 + in4;
     }
     static void Main(string[] args)
     {
- double a1 = getuserinput("give me a 1");
- double a2 = getuserinput("give me a 2");
- double a3 = getuserinput("give me a 3");
- double a4 = getuserinput("give me a 4");
-      /*  Console.WriteLine("Give me a number:");
-        double in1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("give me another number");
-        double in2 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("gimme a third");
-        double in3 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Give me the fourth and final number");
-        double in4 = Convert.ToDouble(Console.ReadLine());*/
 
-    Console.WriteLine("Sum = " + addition(a1,a2));
-    Console.WriteLine("Sum = " + addition(a1,a2,a3));
-   Console.WriteLine("Sum = " + addition(a1,a2,a3,a4));
-    //Console.WriteLine(in1);
+        double a1 = getuserinput("give me a 1");
+        double a2 = getuserinput("give me a 2");
+        double a3 = getuserinput("give me a 3");
+        double a4 = getuserinput("give me a 4");
+        /*  Console.WriteLine("Give me a number:");
+          double in1 = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine("give me another number");
+          double in2 = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine("gimme a third");
+          double in3 = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine("Give me the fourth and final number");
+          double in4 = Convert.ToDouble(Console.ReadLine());*/
+
+        Console.WriteLine("Sum = " + addition(a1, a2));
+        Console.WriteLine("Sum = " + addition(a1, a2, a3));
+        Console.WriteLine("Sum = " + addition(a1, a2, a3, a4));
+        //Console.WriteLine(in1);
     }
 
 }
---
+++ linqtest/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("type the first letter of the person you want to look up");
+string? input = Console.ReadLine();
+
+var contacts = new List<Contact>
+{
+new() {Name = "Magnus", Email = "mms@hotmail.com", Location = "Bø, Telemark"},
+new() {Name = "Per", Email = "Per@person.com", Location = "Tromsø"},
+new() {Name = "Pål", Email = "Pål@pål.pål", Location = "Fredrikstad"},
+new() {Name = "Pia", Email = "Pia@piapål.com", Location = "Fredrikstad"},
+new() {Name = "Mina", Email = "Mina@minimina.com", Location = "Tønsberg"},
+new() {Name = "Elise", Email = "123Elise@hotmail.com", Location = "Bø, Telemark"},
+
+
+};
+var finaList = contacts
+
+.Where(contact => contact.Location == "Fredrikstad" || contact.Location == "Bø, Telemark" || contact.Location == "Tønsberg")
+.Where(contact => contact.Name.StartsWith(input, StringComparison.OrdinalIgnoreCase))
+.Select(contact => new { Email = contact.Email, Name = contact.Name });
+
+foreach (var person in finaList)
+{
+    Console.WriteLine($"1Navn 2Epost: 1:{person.Name} 2:{person.Email}");
+}
+
+
+class Contact
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Location { get; set; }
+}
